9359be00da01d2408b64a862f98f4c474fcab31c
admin <110592726+Web3DappBlockUniversity@users.noreply.github.com>
2026-03-17 10:29:15 +0800

    bump version to v1.0.72

--- install_production.sh
+++ install_production.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo ""
 echo "==============================="
-echo "🚀 SchedulerBot Installer v1.0.71"
+echo "🚀 SchedulerBot Installer v1.0.72"
 echo "==============================="
 echo ""
 
